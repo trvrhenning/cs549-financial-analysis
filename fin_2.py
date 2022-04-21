3b3991b75e48591f93b6ae2e6a10ebb194dcf3f0
Trevor Henning <trvrhenning@gmail.com>
2022-04-20 22:19:06 -0700

    Loaded data from csv on fin_2.py

--- fin_2.py
+++ fin_2.py
@@ -3,5 +3,9 @@
 
 import torch 
 import torch.nn as nn
+import pandas as pd 
 
-#First Load data from CSV File 
+#First Load data from CSV File using pandas this time
+raw_data = pd.read_csv('targetfirm_prediction_dataset_small.csv')
+data_tensor = torch.tensor(raw_data.values)
+print (data_tensor.size(dim = 1)) #should be 18
