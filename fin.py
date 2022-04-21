7dfc910e13a1000e7b3a017f3f0ce3eee10aa878
Trevor Henning <trvrhenning@gmail.com>
2022-04-20 19:39:20 -0700

    Added comments

--- fin.py
+++ fin.py
@@ -1,3 +1,8 @@
+#CS 549 Final Project, Financial Analysis, Group 23
+#This python file implements 3 non sequential machine learning models on financial data from a given csv file
+
 import numpy as np
 import sklearn as sk
 import matplotlib.pyplot as plt 
+
+#First Load data from CSV File 
--- fin_2.py
+++ fin_2.py
@@ -1,2 +1,7 @@
+#CS 549 Final Project, Financial Analysis, Group 23
+#This python file implements 2 sequential machine learning models on financial data from given csv file
+
 import torch 
 import torch.nn as nn
+
+#First Load data from CSV File 
