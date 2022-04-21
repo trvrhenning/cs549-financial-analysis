65cbf7aececb54ee009351e7d05f1b4f9f95c9d7
Trevor Henning <trvrhenning@gmail.com>
2022-04-20 22:07:59 -0700

    Loaded data from csv into numpy array on fin.py

--- fin.py
+++ fin.py
@@ -2,7 +2,19 @@
 #This python file implements 3 non sequential machine learning models on financial data from a given csv file
 
 import numpy as np
+import csv
 import sklearn as sk
 import matplotlib.pyplot as plt 
 
 #First Load data from CSV File 
+raw_data = open('targetfirm_prediction_dataset_small.csv', 'r', newline = '')
+reader  = csv.reader(raw_data, delimiter = ',' , quoting  = csv.QUOTE_NONE)
+readerlist = list(reader)
+
+#get dimmensions
+rowcount = len(readerlist)
+colcount = len(readerlist[0])
+
+data = np.array(readerlist)
+print(data.shape) #225011, 18 
+print(data[0,3]) #should be target column
