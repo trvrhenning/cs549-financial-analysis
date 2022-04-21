388074084e3bd1af044a49451e501ee396e1cc00
Trevor Henning <trvrhenning@gmail.com>
2022-04-21 13:09:51 -0700

    Cut excess data from tensor

--- fin_2.py
+++ fin_2.py
@@ -7,13 +7,21 @@ import pandas as pd
 
 #First Load data from CSV File using pandas this time for ease of use with pytorch
 raw_data = pd.read_csv('targetfirm_prediction_dataset_small.csv')
-data_tensor = torch.tensor(raw_data.values)
+rt_data = torch.tensor(raw_data.values)
 
+#Check if values were loaded correctly
 print()
-print("Checking if data is properly loaded into tensor: \n")
-print ("This value should be 18 : {}".format(data_tensor.size(dim = 1))) #should be 18
-print ("This value should be 6.0 : {}".format(data_tensor[0][0].item())) #should be 6 
+print("Checking if rt_data is properly loaded into tensor: \n")
+print("This value should be 18 : {}".format(rt_data.size(dim = 1))) #should be 18
+print("This value should be 6.0 : {}".format(rt_data[0][0].item())) #should be 6 
 print()
 
+#Slice first unneeded row value column from csv data
+data = rt_data[:rt_data.size(dim = 0),1:rt_data.size(dim =1)] #tensor of size ([225010,17]) 
 
-#Preprocess any data if necessary here
+#Check dimensions and values
+print(data.size()) 
+print("This value should be 1004 : {}".format(data[0][0]))
+print("This value should be 2000 : {}".format(data[0][1]))
+
+#Preprocess data tensor further below if necessary 
