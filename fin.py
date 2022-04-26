b4997c0456694026c7cbb5aca52daf786e3f616c
Trevor Henning <trvrhenning@gmail.com>
2022-04-25 22:13:08 -0700

    cleaning up model evaluation

--- fin.py
+++ fin.py
@@ -40,7 +40,7 @@ Y_data = data[1:data.shape[0],3]
 print("Y_data Shape: {}".format(Y_data.shape))
 
 #Create X, Y Train and X, Y Test 
-X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size = 0.35, shuffle = True)
+X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size = 0.4, shuffle = True)
 
 #Check dimensions of train and test sets 
 print("X_train shape: {}".format(X_train.shape))
@@ -71,11 +71,11 @@ l_reg.fit(X_train, Y_train)
 
 y_pred = l_reg.predict(X_test)
 
-
 #Evalaute Logistic Regression Model 
 cm = metrics.confusion_matrix(Y_test, y_pred) 
-
-print(cm)
+accuracy = (cm[0,0] + cm[1,1])/len(Y_test)
+print("Confusion Matrix for Logistic Regression Model : \n{}".format(cm))
+print("Logistic Regression Model Accuracy : {0:.2%}".format(accuracy))
 
 #2. SVM 
 #Initialize scikit model 
@@ -83,14 +83,13 @@ svm_model = SVC()
 
 #Fit data to our model and make predictions
 svm_model.fit(X_train,Y_train)
-
 y_pred_1 = svm_model.predict(X_test)
 
 #Evaluate SVM model
 cm_1 = metrics.confusion_matrix(Y_test, y_pred_1)
-
-print(cm_1)
-
+accuracy_1 = (cm_1[0,0] + cm_1[1,1])/len(Y_test)
+print("Confusion Matrix for SVM Model : \n{}".format(cm_1))
+print("SVM Model Accuracy : {0:.2%}".format(accuracy_1))
 
 #3. Feed forward Nueral Network
 #Initialize scikit model
@@ -98,10 +97,10 @@ mlp_model = MLPClassifier()
 
 #Fit data to our model and make predictions
 mlp_model.fit(X_train, Y_train)
-
 y_pred_2 = mlp_model.predict(X_test)
 
 #Evaluate Nueral Network Model  
 cm_2 = metrics.confusion_matrix(Y_test, y_pred_2)
-
-print(cm_2)
+accuracy_2 = (cm_2[0,0] + cm_2[1,1])/len(Y_test)
+print("Confusion Matrix for Neural Network Model : \n{}".format(cm_2))
+print("Neural Network Model Accuracy : {0:.2%}".format(accuracy_2))
