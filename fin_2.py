2ebad37b4e2e7dfa5ddb42eec54e40f93e134173
Trevor Henning <trvrhenning@gmail.com>
2022-04-27 11:22:10 -0700

    Split dataset into train and test sets

--- fin_2.py
+++ fin_2.py
@@ -3,27 +3,42 @@
 
 import torch 
 import torch.nn as nn
+import numpy as np
 import pandas as pd 
+from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 
 #First Load data from CSV File using pandas this time for ease of use with pytorch
 raw_data = pd.read_csv('targetfirm_prediction_dataset_small.csv')
 
-# Need to Create train and test sets before this line
-rt_data = torch.tensor(raw_data.values)
-
-#Check if values were loaded correctly
+#Create a numpy array and extract X and Y 
+data = np.array(raw_data.values)
+X_data = np.concatenate((data[:,1:3],data[:,4:18]), axis = 1) 
+Y_data = data[:,3]
 print()
-print("Checking if rt_data is properly loaded into tensor: ")
-print("This value should be 18 : {}".format(rt_data.size(dim = 1))) #should be 18
-print("This value should be 6.0 : {}".format(rt_data[0][0].item())) #should be 6 
+print("Data shape : {} ".format(data.shape))
+print("X_data shape : {}".format(X_data.shape))
+print("Y_data shape : {}".format(Y_data.shape))
 
-#Slice first unneeded row value column from csv data
-data = rt_data[:rt_data.size(dim = 0),1:rt_data.size(dim =1)] #tensor of size ([225010,17]) 
+#Create X, Y Train and X, Y Test before converting to tensors
+Xtr, Xte, Ytr, Yte = train_test_split(X_data, Y_data, test_size = 0.35, shuffle = True)
 
-#Check dimensions and values
-print(data.size()) 
-print("This value should be 1004 : {}".format(data[0][0]))
-print("This value should be 2000 : {}".format(data[0][1]))
+#Convert to tensor dataset and then use dataloader
+X_train = torch.tensor(Xtr)
+Y_train = torch.tensor(Ytr)
+X_test = torch.tensor(Xte)
+Y_test = torch.tensor(Yte)
 
-#Create RNN class 
+train_set = TensorDataset(X_train, Y_train)
+test_set = TensorDataset(X_test, Y_test) 
+
+#Load with dataloader here, need to get batch size and other params as well
+train = DataLoader(train_set, batch_size = 512, shuffle = False)
+test = DataLoader(test_set, batch_size = 512, shuffle = False)
+
+print("X_train shape: {}".format(X_train.size()))
+print("X_test shape: {}".format(X_test.size()))
+print("Y_train shape: {}".format(Y_train.size()))
+print("Y_test shape: {}".format(Y_test.size()))
+
+#Create RNN classes
