78d2f5e431c02f14b33e0e230e900b6fb1bc18eb
Trevor Henning <trvrhenning@gmail.com>
2022-05-09 21:46:14 -0700

    Created sequences

--- fin_2.py
+++ fin_2.py
@@ -16,9 +16,10 @@ data = np.array(raw_data.values)
 #slice unneeded column
 data = data[:,1:data.shape[1]]
 
-#split data by company ID
-def split_data(data_m):
+#split data by company ID and then seperate into sequences, may need to remove last row for test data
+def split_data(data_m, seq_len):
     new_data_m = [ ]
+    sequences = [ ]
     bucket = [20] 
     company_id = data_m[0,0]
     idx = 0 
@@ -32,82 +33,52 @@ def split_data(data_m):
             headidx = idx
             bucket.clear()
             idx+=1 
+    for i in range(len(new_data_m)):
+        if(new_data_m[i].shape[0] < seq_len):
+            sequences.append(new_data_m[i])
+        else:
+            sequences.append(new_data_m[i][-seq_len:,:])
     #returns list of numpy arrays seperated by company id
-    return new_data_m
+    return sequences
 
-#list of numpy arrays seperated by company id
-new_data = np.array(split_data(data), dtype = object)
-test_size = int(np.round(0.3*new_data.shape[0]))
+#list of numpy arrays seperated by company id each in length of chosen sequence lenght
+new_data = np.array(split_data(data, 5), dtype = object)
+test_size = int(np.round(0.3 * new_data.shape[0]))
 train_data = new_data[:(new_data.shape[0] - test_size)]
 test_data = new_data[(new_data.shape[0] - test_size):new_data.shape[0]]
-print(f"Check train data shape: {train_data.shape}")
+print(f"Check train data shape: {train_data.shape}") #size should be amount of sequences
 
-#train_data[i].shape and test_data[i].shape = (samples_each_company, features)
-#need to slice each companys data ie. train_data[i] by a max sequence length, but smaller if less length than max sequence
+#train_data[i].shape and test_data[i].shape = (samples_each_company(size of sequence length), features) 
 #features, 1-3 are company id year and target val, thus we dont wnat those in calculations for training
 
-#below code will need to be changed, but process of converting to tensors should be similar. 
-
-X_data = np.concatenate((data[:,1:3],data[:,4:18]), axis = 1) 
-Y_data = data[:,3]
-print()
-print("Data shape : {} ".format(data.shape))
-print("X_data shape : {}".format(X_data.shape))
-print("Y_data shape : {}".format(Y_data.shape))
-
-#Create X, Y Train and X, Y Test before converting to tensors
-Xtr, Xte, Ytr, Yte = train_test_split(X_data, Y_data, test_size = 0.35, shuffle = True)
-
-#Convert to tensor dataset and then use dataloader
-X_train = torch.tensor(Xtr)
-Y_train = torch.tensor(Ytr)
-X_test = torch.tensor(Xte)
-Y_test = torch.tensor(Yte)
-
-train_set = TensorDataset(X_train, Y_train)
-test_set = TensorDataset(X_test, Y_test) 
-
-#Load with dataloader here, need to get batch size and other params as well
-train = DataLoader(train_set, batch_size = 64, shuffle = False)
-test = DataLoader(test_set, batch_size = 64, shuffle = False)
-
-print("X_train shape: {}".format(X_train.size()))
-print("X_test shape: {}".format(X_test.size()))
-print("Y_train shape: {}".format(Y_train.size()))
-print("Y_test shape: {}".format(Y_test.size()))
-
-#Create RNN classes
-
+batch_size = 64
 input_size = 14
 hidden_size = 100 
 num_layers = 2 
 output_size = 1 
 num_epochs = 100 
 
-#class LSTM
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size)
-    super(LSTM, self).__init__()
-    self.hidden_size = hidden_size 
-    self input_size = input_size
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTM, self).__init__()
+        self.hidden_size = hidden_size 
+        self.input_size = input_size
     
-    self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-    self.fc = nn.Linear(hidden_size, output_size)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
     
     def forward(self,x): 
         #fill in code here 
         pass
     
-#class GRU
-
 class GRU(nn.Module):
-    def __init__(self, input, hidden_size,num_layers, output_size)
-    super(GRU, self).__init__()
-    self.hidden_size = hidden_size 
-    self input_size = input_size
+    def __init__(self, input, hidden_size,num_layers, output_size):
+        super(GRU, self).__init__()
+        self.hidden_size = hidden_size 
+        self.input_size = input_size
     
-    self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
-    self.fc = nn.Linear(hidden_size, output_size) 
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size) 
     
     def forward(self, x):
         #fill in code here
